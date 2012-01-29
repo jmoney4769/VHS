7213c5bb89aea1097485bba83318c0904225eb00
Jared Moore <jaredmoore47@gmail.com>
2012-01-29 10:18:59 -0500

    Added my methods to the group project

--- src/govhs/apsc/week19/GroupProjectOfficial.java
+++ src/govhs/apsc/week19/GroupProjectOfficial.java
@@ -4,9 +4,7 @@ package govhs.apsc.week19; // ignore this, I use packages with eclipse to organi
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class GroupProjectOfficial {
 
